9304746aa17fc49a447d6fb917c3a6d78b58e084
Sam-Melius <91688565+Sam-Melius@users.noreply.github.com>
2022-04-01 14:37:58 -0700

    set up sql tables, and frontend

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,15 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS posts CASCADE;
+
+CREATE TABLE users (
+    github_username TEXT NOT NULL PRIMARY KEY,
+    photo_url TEXT NOT NULL
+);
+
+CREATE TABLE posts (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    text TEXT NOT NULL,
+    username TEXT REFERENCES users(github_username)
+);
